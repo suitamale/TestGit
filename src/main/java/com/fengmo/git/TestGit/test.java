fb99ebe0e6f25a7f85332f9a135ec0700696864c
suitamale <suitamale@bigdata.com>
2021-08-10 21:23:55 +0800

    34

--- src/main/java/com/fengmo/git/TestGit/test.java
+++ src/main/java/com/fengmo/git/TestGit/test.java
@@ -2,7 +2,8 @@ package com.fengmo.git.TestGit;
 
 public class test {
 	public void main(String[] str) {
-		System.out.println("left>>>>");
+		System.out.println("right>>>>");
+		System.out.println("right.txt>>>>");
 	}
 
 }
